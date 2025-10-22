0197543c5d464aecb6da831cfbec8d0a74834a00
Atai Ibragimov <aibragimov2003@bk.ru>
2025-10-22 03:24:13 +0200

    first commit

---
+++ src/main/java/com/example/Atai_Ibragimov/AtaiIbragimovApplication.java
@@ -0,0 +1,13 @@
+package com.example.Atai_Ibragimov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaiIbragimovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaiIbragimovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Atai_Ibragimov/AtaiIbragimovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Atai_Ibragimov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtaiIbragimovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
